53cc94d26d9f3551dcbaaf5cd862078a4f0e1cf3
Santhosh-Kumar-99 <60814662+Santhosh-Kumar-99@users.noreply.github.com>
2023-02-16 02:35:52 +0530

    Commit V1 React router demo

--- src/App.js
+++ src/App.js
@@ -1,24 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { Route, Routes } from "react-router-dom";
+import About from "./components/About";
+import Home from "./components/Home";
+import Error from "./components/Error";
+import NavBar from "./components/NavBar";
+import OrderSummary from "./components/OrderSummary";
+import Products from "./components/Products";
+import FeaturedProduct from "./components/FeaturedProduct";
+import NewProduct from "./components/NewProduct";
+import Users from "./components/Users"
+import UserDetails from "./components/UserDetails";
+import Admin from "./components/Admin";
 
 function App() {
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <NavBar />
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path="about" element={<About />} />
+        <Route path="order-summary" element={<OrderSummary />} />
+        <Route path="products" element={<Products />}>
+          <Route index element={<div>Select feature or new product</div>} />
+          <Route path="featured" element={<FeaturedProduct />} />
+          <Route path="new" element={<NewProduct />} />
+        </Route>
+        <Route path="users" element={<Users />}>
+          <Route path=":userId" element={<UserDetails />} />
+          <Route path="admin" element={<Admin />} />
+        </Route>
+        <Route path="*" element={<Error />} />
+      </Routes >
+    </>
   );
 }
 
